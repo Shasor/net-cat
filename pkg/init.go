e8ab55feab106ce4dbd14f90e765cc327f5905da
Adam GONCALVES <goncalvesadam@icloud.com>
2024-09-02 10:21:46 +0200

    add init const to pkg/ + add pkg package to go.work

---
+++ pkg/init.go
@@ -0,0 +1,10 @@
+package pkg
+
+const (
+	maxConnections = 10
+	DefaultPort    = "8989"
+)
+
+var history = ""
+
+var welcomeMsg = "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\"\n[ENTER YOUR NAME]: "
